08ca1fe472b4f7fc904d98a4030f7d8bcb0b9552
AlexMos1n <alex-mos777@yandex.ru>
2024-05-12 15:43:37 +0300

    Edit README.md, types/index.ts and add README-UML.png

---
+++ src/types/index.ts
@@ -0,0 +1,88 @@
+import { IEvents } from "../components/base/events";
+
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}
+
+export interface IProductsData {
+  products: IProduct[];
+  addProduct(product: IProduct): void;
+  getProduct(productId: string): IProduct;
+}
+
+export interface ICustomer {
+  payment: TPayment;
+  address: string;
+  email: string;
+  phone: string;
+  purchases: IProduct[];
+  totalSum: number;
+}
+
+export interface IOrderData extends ICustomer {
+  getCustomerInfo(): ICustomer;
+  setCustomerInfo(userData: ICustomer): void;
+  }
+
+export interface ISuccessData {
+  id: string;
+  total: number;
+}
+
+export interface IBasketHeader {
+  counter: number;
+}
+
+export interface IModal {
+  content: HTMLElement;
+  open(): void;
+  close(): void;
+}
+
+export interface IForm {
+  valid: boolean;
+  textError: string;
+  form: HTMLFormElement;
+  clear(): void;
+}
+
+export interface ICard {
+  card: HTMLElement;
+}
+
+export interface ICardConstructor {
+  new (container: HTMLElement, events: IEvents): ICard;
+}
+
+export interface ICardsCatalog {
+  addCard(cardsList: TCard[]): void;
+}
+
+export interface ICardBasket {
+  card: HTMLElement;
+  delCard(): void;
+}
+
+export interface ICardBasketConstructor {
+  new (container: HTMLElement, events: IEvents): ICardBasket;
+}
+
+export interface IBasketView {
+  list: ICardBasket[];
+  valid: boolean;
+  totalPrice: number;
+}
+
+export interface Success {
+  total: number;
+}
+
+export type TPayment = 'online' | 'point';
+export type TCard = Omit<IProduct, 'description'>;
+export type TCardBasket = Pick<IProduct, 'title'|'price'>;
+export type TSuccess = Pick<ISuccessData, 'total'>
